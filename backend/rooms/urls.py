f46875ebd14e97d9d67493cae9d34df788231762
ANDREI DYDYSHKA <dydyshko1999@gmail.com>
2022-03-01 17:21:21 +0300

    Add get_room_players view

---
+++ backend/rooms/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('list/', views.RoomListView.as_view(), name='rooms'),
+    path('<int:pk>/', views.RoomDetailView.as_view(), name='room'),
+    path('<int:pk>/players/', views.get_room_players, name='room'),
+]
--- backend/rooms/views.py
+++ backend/rooms/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Room
+from .serializers import RoomSerializer
+from core.views import BaseListView, BaseDetailView
+from users.models import Player
+from users.serializers import PlayerListSerializer
+
+
+class RoomListView(BaseListView):
+    model = Room
+    serializer_class = RoomSerializer
+
+
+class RoomDetailView(BaseDetailView):
+    model = Room
+    serializer_class = RoomSerializer
+
+
+@api_view(['GET'])
+def get_room_players(request, pk):
+    room = get_object_or_404(Room, pk=pk)
+    player_ids = room.playerroom_set.values("player")
+    players = Player.objects.filter(pk__in=player_ids)
+    serializer = PlayerListSerializer(players, many=True)
+    return Response(serializer.data)
