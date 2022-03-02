27b281e2e484e2ec9976d67dc2da55a3033ca1bc
ANDREI DYDYSHKA <dydyshko1999@gmail.com>
2022-03-02 14:19:29 +0300

    Fix lilbug in signals

--- backend/users/signals.py
+++ backend/users/signals.py
@@ -2,7 +2,8 @@ from django.db.models.signals import pre_save
 from django.contrib.auth.models import User
 
 
-def update_user(sender, user, **kwargs):
+def update_user(sender, instance, **kwargs):
+    user = instance
     if user.email:
         user.username = user.email
 
