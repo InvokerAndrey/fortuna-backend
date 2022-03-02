cf28b3596b98b024ade9f4ca62c00243c0535d7f
ANDREI DYDYSHKA <dydyshko1999@gmail.com>
2022-03-02 17:21:03 +0300

    Fix rooms urls

--- backend/rooms/urls.py
+++ backend/rooms/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('list/', views.RoomListView.as_view(), name='rooms'),
     path('<int:pk>/', views.RoomDetailView.as_view(), name='room'),
-    path('<int:pk>/players/', views.get_room_players, name='room'),
+    path('<int:pk>/players/', views.get_room_players, name='room-players'),
 ]
