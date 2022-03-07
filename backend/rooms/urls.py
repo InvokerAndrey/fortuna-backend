03d5646fa65a1dbbd12ca2f4dabadc1e7bbe7547
Andrey <dydyshko1999@gmail.com>
2022-03-07 13:00:04 +0300

    Create and delete room endpoints

--- backend/rooms/urls.py
+++ backend/rooms/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
     path('list/', views.RoomListView.as_view(), name='rooms'),
+    path('add/', views.RoomDetailView.as_view(), name='add'),
     path('<int:pk>/', views.RoomDetailView.as_view(), name='room'),
     path('<int:pk>/players/', views.get_room_players, name='room-players'),
+    path('<int:pk>/delete/', views.delete_room, name='delete-room'),
 ]
--- backend/rooms/views.py
+++ backend/rooms/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework import status
 
 from .models import Room
 from .serializers import RoomSerializer
@@ -19,6 +20,16 @@ class RoomDetailView(BaseDetailView):
     model = Room
     serializer_class = RoomSerializer
 
+    @permission_classes([IsAdminUser])
+    def post(self, request):
+        data = request.data
+        Room.objects.create(
+            name=data['name'],
+            description=data.get('description'),
+            website=data.get('website')
+        )
+        return Response(status=status.HTTP_201_CREATED)
+
 
 @api_view(['GET'])
 @permission_classes([IsAdminUser])
@@ -28,3 +39,11 @@ def get_room_players(request, pk):
     players = Player.objects.filter(pk__in=player_ids)
     serializer = PlayerListSerializer(players, many=True)
     return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def delete_room(request, pk):
+    room = get_object_or_404(Room, pk=pk)
+    room.delete()
+    return Response(status=status.HTTP_202_ACCEPTED)
